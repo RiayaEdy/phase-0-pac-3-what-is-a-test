147e609bc7aedaabe9dc4e847d65d06c727761c1
RiayaEdy <edywidowaty@gmail.com>
2022-08-02 22:17:41 +1000

    testing lab done

--- test/index-test.js
+++ test/index-test.js
@@ -1,25 +1,25 @@
 const { name, height, message } = require("../index.js");
 
-/*
+const index = require("../index")
+
 describe('what-is-a-test', () => {
   describe('Name', () => {
     it('returns "Susan"', () => {
-      expect(name).toEqual('Susan')
+      expect(index.name).toEqual('Susan')
     })
   })
 
 
   describe('Height', () => {
     it('is less than 40', () => {
-      expect(height).toBeLessThan(40)
+      expect(index.height).toBeLessThan(40)
     })
   })
 
   describe('Message', () => {
     it('gives the name and height', () => {
-      expect(message).toInclude(name)
-      expect(message).toInclude(height)
+      expect(index.message).toInclude(name)
+      expect(index.message).toInclude(height)
     })
   })
 })
-*/
